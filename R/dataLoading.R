61e9efed103c190fc50d3c311203947c85b09e85
David Landy <dhlandy@gmail.com>
2018-02-27 17:18:40 -0500

    first minimal working version with tests

--- R/dataLoading.R
+++ R/dataLoading.R
@@ -1,17 +1,23 @@
 currentGMVersion = "2.5.12"
 
 #' importGraspableJson
-#' @param fileName a vector of stimuli, between 0 and inf
+#' This simple script just loads a json data file, merges the trial data and events, and renames a few columns
+#' Optional, it also twiddles things down to a single version of GM, and a single context ("experiment").  
+#' 
+#' @param fileName the name of a file locating a json project
+#' @param version If any non-false value, data will be limited to versions of gm matching this string
+#' @param limitExperiment If not false, limit to experiment contexts matching this value
 #' Comments
 #' 
-#' @return A list of Tibbles
+#' @return a tibble containing the data from the file, with the class "GM X", where X is the version 
+#' specified (or the first version number to appear)
 #' @seealso 
 #' @export
 #' @examples
 #' canvasSnippetFile <- system.file("extdata", "canvasSnippet.json", package = "GraspableDataAnalysis", mustWork = TRUE)
-#' canvasSnippetData <- importGraspableJson(canvasSnippetFile, singleVersionOnly = "2.5.12" )
+#' canvasSnippetData <- importGraspableJson(canvasSnippetFile, version = "2.5.12" )
 #' 
-importGraspableJson <- function(filename, singleVersionOnly = F, limitExperiment = F){
+importGraspableJson <- function(filename, version = F, limitExperiment = F){
   graspableJSON <- jsonlite::fromJSON(read_file(filename), simplifyDataFrame=T, flatten=T)
   gmData <- as.tibble(graspableJSON$data)
   trials <- as.tibble(graspableJSON$trials)
@@ -24,8 +30,15 @@ importGraspableJson <- function(filename, singleVersionOnly = F, limitExperiment
   )
   
   
-  if(singleVersionOnly != F){
-    graspableJSON <- graspableJSON %>% filter(gm_version==singleVersionOnly)
+  if(version != F){
+    graspableJSON <- graspableJSON %>% filter(gm_version==version)
+    class(graspableJSON) <- append(paste("gm", version)
+                                   , class(graspableJSON))
+    
+  } else {
+    class(graspableJSON) <- append(paste("gm", graspableJSON$gm_version[1])
+                                   , class(graspableJSON))
+    
   }
   if(limitExperiment != F){
     graspableJSON <- graspableJSON %>% filter(experiment_id %in% limitExperiment)
